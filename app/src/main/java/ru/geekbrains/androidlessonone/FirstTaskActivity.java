be1fdc8167131a2ef89e9b09304b30846666da07
Alecsey Efimov <alicsey2000@gmail.com>
2022-06-20 07:27:51 +0300

    Подготовка макета(first_task). Добавлены <LinearLayout/> и прочие представления, необходимые для выполнения первого задания. У всех представлений заданы обязательные атрибуты.

--- app/src/main/java/ru/geekbrains/androidlessonone/FirstTaskActivity.java
+++ app/src/main/java/ru/geekbrains/androidlessonone/FirstTaskActivity.java
@@ -3,11 +3,11 @@ package ru.geekbrains.androidlessonone;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_task);
     }
 }
